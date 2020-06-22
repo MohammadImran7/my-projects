7e9e872ceb8ed7ad05a27df1b26def83f04bf1a6
Mohammad <iosdevmohammad@gmail.com>
2020-06-23 00:21:56 +0530

    Initial commit

---
+++ LocalityDetails/LocalityDetails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalityDetails
+//
+//  Created by fkhader on 6/23/20.
+//  Copyright © 2020 fkhader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
